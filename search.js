7db2b84eeec465c0bd450c56f16fe3c66e44be66
kelvin13 <kelvin13ma@gmail.com>
2022-03-29 17:53:29 -0500

    initial commit

---
+++ search.js
@@ -0,0 +1,127 @@
+const searchbar = document.getElementById('search');
+const input     = document.getElementById('search-input');
+const output    = document.getElementById('search-results');
+
+var search      = null;
+var unloaded    = true; 
+var selection   = null;
+var pending     = null;
+
+function select(index) {
+    output.children.item(selection.index).classList.remove('selected');
+    output.children.item(index).classList.add('selected');
+    selection.index = index;
+}
+
+function reinitialize() {
+    if (unloaded === false) {
+        if (selection !== null) {
+            select(0);
+        }
+        return; 
+    } 
+    unloaded = false;
+    
+    var requests = [];
+    for (const package of includedPackages) {
+        requests.push(fetch('/reference/' + package + '/search.json').then((response) => response.json()));
+    }
+    
+    Promise.all(requests).then(function(json) {
+        const symbols = json.flat(1);
+        search = {
+            symbols: symbols, 
+            index: lunr(function() {
+                this.ref('i');
+                this.field('text');
+                
+                // disable stemming 
+                this.pipeline.remove(lunr.stemmer);
+                this.searchPipeline.remove(lunr.stemmer);
+                
+                for (let i = 0; i < symbols.length; i++) {
+                    this.add({i: i, text: symbols[i].text});
+                }
+            })
+        };
+        if (pending !== null) {
+            suggest(pending);
+        }
+    })
+    .catch(function(error) {
+        unloaded = true;
+        console.error('error:', error);
+    });
+}
+function suggest(event) {
+    if (search === null) {
+        pending = event;
+        return;
+    } else {
+        pending = null;
+    }
+    const needle    = event.target.value.toLowerCase();
+    
+    var results     = undefined;
+    if (needle.length > 0) {
+        results     = search.index.query(function(query) {
+            query.term(needle, { boost: 100});
+            query.term(needle, { boost:  10, wildcard: lunr.Query.wildcard.TRAILING });
+            query.term(needle, { boost:   5, wildcard: lunr.Query.wildcard.TRAILING, editDistance: 1 });
+            query.term(needle, { boost:   1, wildcard: lunr.Query.wildcard.TRAILING, editDistance: 2 });
+        });
+    } else {
+        results     = [];
+    }
+    
+    const best = results.slice(0, 10);
+    output.replaceChildren();
+    for (const result of best) {
+        const symbol    = search.symbols[result.ref];
+        
+        const item      = document.createElement("li");
+        const anchor    = document.createElement("a");
+        anchor.setAttribute("href", symbol.uri);
+        const text      = document.createTextNode(symbol.title);
+        anchor.appendChild(text);
+        item.appendChild(anchor);
+        output.appendChild(item);
+    }
+    if (best.length > 0) {
+        selection = {index: null, choices: best};
+        select(0);
+    } else {
+        selection = null;
+    }
+}
+function navigate(event) {
+    if (event.keyCode == 38) {
+        if (selection === null) {
+            return;
+        } else if (selection.index > 0) {
+            select(selection.index - 1);
+            event.preventDefault();
+            return false;
+        }
+    } else if (event.keyCode == 40) {
+        if (selection === null) {
+            return;
+        } else if (selection.index < selection.choices.length - 1) {
+            select(selection.index + 1);
+            event.preventDefault();
+            return false;
+        }
+    }
+}
+function follow(event) {
+    event.preventDefault();
+    if (selection === null) {
+        return false;
+    }
+    window.location.assign(search.symbols[selection.choices[selection.index].ref].uri);
+}
+
+input.addEventListener('focus', reinitialize); 
+input.addEventListener('input', suggest); 
+input.addEventListener('keydown', navigate); 
+searchbar.addEventListener('submit', follow); 
